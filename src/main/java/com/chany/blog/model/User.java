dc0d354333ea828b3a4d5b4b169291a187c3aa44
Chany <inshakr2@naver.com>
2022-02-26 18:19:57 +0900

    Entity 생성 - User

---
+++ src/main/java/com/chany/blog/model/User.java
@@ -0,0 +1,30 @@
+package com.chany.blog.model;
+
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 30)
+    private String username;
+
+    @Column(nullable = false, length = 100)
+    private String password;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role = Role.USER;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
