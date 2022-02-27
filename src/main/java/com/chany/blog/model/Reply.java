af2c35eb20b6c0f23f2f532d28d3bcd042ce147f
Chany <inshakr2@naver.com>
2022-02-27 15:49:15 +0900

    Entity 생성 - Reply

---
+++ src/main/java/com/chany/blog/model/Reply.java
@@ -0,0 +1,34 @@
+package com.chany.blog.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity @Data @Builder
+public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false, length = 200)
+    private String content;
+
+    @ManyToOne
+    @JoinColumn(name = "boardId")
+    private Board board;
+
+    @ManyToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @CreationTimestamp
+    private Timestamp createDate;
+}
