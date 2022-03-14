35adc2157bc32b6caf220e01084874011cb1bbc4
Chany <inshakr2@naver.com>
2022-03-15 00:07:20 +0900

    회원 정보 수정 (session 추가)

--- src/main/java/com/chany/blog/config/SecurityConfig.java
+++ src/main/java/com/chany/blog/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.chany.blog.config.auth.PrincipalDetailService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -24,12 +25,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(principalDetailService).passwordEncoder(encodePWD());
     }
-
     // 미인증 유저는 /auth/* , index.jsp , static이하 /js/* , /css/* , /image/* 까지만 허용
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
